e0870315ba169f7559d6042aff7fdc6d0e2ee1a8
sunitsai <58470631+sunitsai@users.noreply.github.com>
2022-06-20 08:46:28 +0530

    Final Commit

---
+++ test.js
@@ -0,0 +1,25 @@
+Ecommerce = 
+{
+    Fashion:{
+        MensWear:{
+            Tshirt:[Casual Tshirt,Sport Tshirt,Polo Tshirt,Round Neck Tshirt],
+            Footwear:[Sport Shoes,Formal Shoes,Casual Shoes],
+            Jeans:[Levis, Pepe Jeans, Lee, Spykar]
+        },
+        KidsWear:{
+            Footwear:{
+                Heels:[Boots, Wedges,Platforms],
+                Shoes:[Sport Shoes, Casual Shoes, Ballets, Boots]
+            },
+            Tshirt:[Casual Tshirt,Sport Tshirt,Polo Tshirt,Round Neck Tshirt],
+        }
+    },
+    Mobile:{
+        Case:[Xiaomi,Samsung,Oneplus,Apple],
+        PowerBank:[MI, OnePlus, Philips, Redmi],
+        Brands:[Apple,Oppo,Vivo,MI,Xiaomi,Samsung,{
+            price:[55000,45000,35000,30000,25000,20000]
+        }]
+    },
+
+}
